dae12636b68b9aecfdc9500168eb1edece613a38
unknown <ibraheemhaseeb7@gmail.com>
2023-04-25 21:32:58 +0500

    first push

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  // @ts-nocheck
+
+  let response = '';
+  let url = ''
+
+  async function checkApi() {
+    await fetch(url, {
+      method: "GET", headers: {"Content-type": "applicaton/json"}
+    }).then((res) => res.json()).then((res) => response = JSON.stringify(res))
+  } 
+</script>
+
+<main>
+  <h1>API Tester</h1>
+  <input type="url" placeholder="Enter API endpoint link..." value={url} on:change={(e)=>url=e.target.value} />
+  <button on:click={checkApi}>check</button>
+  <div>API RESPONDED: <br />{response}</div>
+</main>
+
+<style>
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 1rem;
+    transform: translateY(-2rem);
+  }
+</style>
